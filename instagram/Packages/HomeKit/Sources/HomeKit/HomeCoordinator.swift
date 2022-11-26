6eb5b18d1b2e0533fe677e75ea879937d8ca0387
Renan Marchini Andrusiac <renan.andrusiac@btgpactual.com>
2022-11-26 17:16:56 -0300

    Create HomeCoordinator class

---
+++ instagram/Packages/HomeKit/Sources/HomeKit/HomeCoordinator.swift
@@ -0,0 +1,15 @@
+//
+//  HomeCoordinator.swift
+//  
+//
+//  Created by Renan Andrusiac on 26/11/22.
+//
+
+import Foundation
+
+// MARK: - HomeCoordinator
+
+class HomeCoordinator {
+
+    
+}
--- instagram/Packages/HomeKit/Sources/HomeKit/HomeKit.swift
+++
@@ -1,6 +0,0 @@
-public struct HomeKit {
-    public private(set) var text = "Hello, World!"
-
-    public init() {
-    }
-}
